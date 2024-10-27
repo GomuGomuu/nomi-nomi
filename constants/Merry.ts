44493249e3c7f48457cd928dd478894ca5931282
Vitor José Stähelin <vitorjosestahelin@gmail.com>
2024-10-27 20:30:52 -0300

    fix: add MerryEndpoints constants for API URLs

---
+++ constants/Merry.ts
@@ -0,0 +1,7 @@
+import { MERRY_API_URL } from "@env";
+
+export const MerryEndpoints = {
+  BASE_URL: MERRY_API_URL,
+  PING: `${MERRY_API_URL}/ping/`,
+  RECOGNITION: `${MERRY_API_URL}/recognition/`,
+};
